dce71aade0bfe6d0c7639eef951777e218d62bee
zhangyan <1843447064@qq.com>
2018-09-20 09:38:26 +0800

    9-20

--- router/order.js
+++ router/order.js
@@ -1,21 +1,11 @@
-const Router=require('express').Router;
+ const Router=require('express').Router;
 const router=Router();
 const UserModel = require('../models/userModle.js');
 const OrderModel = require('../models/order.js');
 
-//权限控制
-// router.use((req,res,next)=>{
-// 	if(req.userInfo.isAdmin){
-// 		next()
-// 	}else{
-// 		res.send({
-// 			code:10,
-// 		});
-// 	}
-// })
 
 
-//获取生成订单的商品列表
+//获取商品列表
 router.get('/getOrderProductList',(req,res)=>{
 	UserModel.findById(req.userInfo._id)
 	.then(user=>{
@@ -38,12 +28,12 @@ router.get('/getOrderProductList',(req,res)=>{
 })
 
 //获取订单信息
-router.get('/list',(req,res)=>{
+router.get('/home/list',(req,res)=>{
 	let page = req.query.page;
 	let query = {
 		user:req.userInfo._id
 	}
-	OrderModel.getPaginationOders(page,query)
+	OrderModel.getPaginationOrders(page,query)
 	.then((result)=>{
 		res.json({
 			code:0,
@@ -63,6 +53,18 @@ router.get('/list',(req,res)=>{
 	})
 })
 
+ //获取订单详情页
+router.get('/home/detail',(req,res)=>{
+ 
+ 	OrderModel
+ 	.findOne({orderNo:req.query.orderNo,user:req.userInfo._id})
+	.then(data=>{
+		res.json({
+			code :0,
+			data:data
+		})
+	})
+});
 //创建订单
 router.post('/',(req,res)=>{
 	UserModel.findById(req.userInfo._id)
@@ -136,19 +138,18 @@ router.post('/',(req,res)=>{
 })
 
  //获取购物订单
- router.get('/getlist',(req,res)=>{
- 	let page = req.query.page;
- 	let query = {
- 		user:req.userInfo._id
- 	}
+router.get('/getlist',(req,res)=>{
+	let page = req.query.page;
+	let query = {
+					user:req.userInfo._id
+				}
 
- 	OrderModel
- 	.getPaginationProduct(page,query)
+	OrderModel
+	.getPaginationProduct(page,query)
 	.then(data=>{
-		
+
 		res.json({
 			code :0,
-			
 			data:{
 				list:data.list,
 				current:data.current,
@@ -158,31 +159,151 @@ router.post('/',(req,res)=>{
 			}
 		})
 	})
- });
+});
+//搜索
+router.get('/search',(req,res)=>{
+	let page = req.query.page || 1;
+	let keyword = req.query.keyword
+	OrderModel
+	.getPaginationOrders(page,{orderNo:{$regex:new RegExp(keyword,'i')}})
+	.then((result)=>{
+		// console.log(result)
+		res.json({ 
+			code:0,
+			data:{
+				current:result.current,
+				total:result.total,
+				list:result.list,
+				pageSize:result.pageSize,
+				keyword:keyword
+			}
+		});	 
+	})
+	.catch(e=>{
+		res.json({
+			code:1,
+			message:'查找订单失败,数据库操作失败'
+		})
+	});
+})
 
- //获取订单详情页
- router.get('/detail',(req,res)=>{
- 
- 	OrderModel
- 	.findOne({orderNo:req.query.orderNo,user:req.userInfo._id})
+ //取消订单
+router.put('/cancel',(req,res)=>{
+	OrderModel
+	.findOneAndUpdate(
+			{orderNo:req.body.orderNo,user:req.userInfo._id},
+			{status:"20",statusDesc:"取消"},
+			{new :true}
+		)
 	.then(data=>{
+	
 		res.json({
 			code :0,
 			
 			data:data
 		})
 	})
- });
- //取消订单
- router.put('/cancel',(req,res)=>{
+	.catch(e=>{
+		res.json({
+			code :0,
+			
+			message:"取消订单失败"
+		})
+	})
+})
+
+// 权限控制
+router.use((req,res,next)=>{
+	if(req.userInfo.isAdmin){
+		next()
+	}else{
+		res.send({
+			code:10,
+		});
+	}
+})
+
+//后台获取所有订单
+router.get('/',(req,res)=>{
+	let page = req.query.page;
+	OrderModel.getPaginationOrders(page )
+	.then((result)=>{
+		res.json({
+			code:0,
+			data:{
+				current:result.current,
+				total:result.total,
+				list:result.list,
+				pageSize:result.pageSize
+			}
+		})
+	})
+	.catch((e)=>{
+		res.json({
+			code:1,
+			message:'获取订单列表失败'
+		})
+	})
+})
+//后台搜索
+router.get('/search',(req,res)=>{
+	let page = req.query.page || 1;
+	let keyword = req.query.keyword;
+	OrderModel
+	.getPaginationOrders(page,{
+		orderNo:{$regex:new RegExp(keyword,'i')}
+	})
+	.then((result)=>{
+		// console.log(result)
+		res.json({ 
+			code:0,
+			data:{
+				current:result.current,
+				total:result.total,
+				list:result.list,
+				pageSize:result.pageSize,
+				keyword:keyword
+			}
+		});	 
+	})
+	.catch(e=>{
+		res.json({
+			code:1,
+			message:'查找订单失败,数据库操作失败'
+		})
+	});
+})
+ //后台获取订单详情页
+router.get('/detail',(req,res)=>{
+ 	
+ 	OrderModel
+ 	.findOne({orderNo:req.query.orderNo})
+	.then(order=>{
+		res.json({
+			code :0,
+			data:order
+		})
+	})
+	.catch(e=>{
+		res.json({
+			code :1,
+			message:'后台获取订单详情出错'
+		})
+	})
+});
+
+
+ //发货按钮
+
+ router.put('/deliver',(req,res)=>{
+ 		console.log(req)
  		OrderModel
  		.findOneAndUpdate(
- 			{orderNo:req.body.orderNo,user:req.userInfo._id},
- 			{status:"20",statusDesc:"取消"},
+ 			{orderNo:req.body.orderNo},
+ 			{status:"40",statusDesc:"已发货"},
  			{new :true}
  			)
 		.then(data=>{
-		
 			res.json({
 				code :0,
 				
@@ -197,7 +318,6 @@ router.post('/',(req,res)=>{
 			})
 		})
 	})
-
 
 
 module.exports = router;
